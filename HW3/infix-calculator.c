5a750d151dc8aab55d04e69ec8be2ef523d1ea7a
Nare <hnare4378@gmail.com>
2025-12-07 18:38:12 +0400

    Changed some things again

--- HW3/infix-calculator.c
+++ HW3/infix-calculator.c
@@ -2,17 +2,37 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-#define MAX 100
+int *num_stack = NULL;
+int num_size = 0;
+int num_top = -1;
 
-int num_stack[MAX];
-char op_stack[MAX];
-int num_top = -1, op_top = -1;
+void push_num(int x) {
+    if (num_top + 1 >= num_size) {
+        num_size = num_size == 0 ? 8 : num_size * 2;
+        num_stack = realloc(num_stack, num_size * sizeof(int));
+    }
+    num_stack[++num_top] = x;
+}
 
-void push_num(int x) { num_stack[++num_top] = x; }
-int pop_num() { return num_stack[num_top--]; }
+int pop_num() {
+    return num_stack[num_top--];
+}
 
-void push_op(char c) { op_stack[++op_top] = c; }
-char pop_op() { return op_stack[op_top--]; }
+char *op_stack = NULL;
+int op_size = 0;
+int op_top = -1;
+
+void push_op(char c) {
+    if (op_top + 1 >= op_size) {
+        op_size = op_size == 0 ? 8 : op_size * 2;
+        op_stack = realloc(op_stack, op_size);
+    }
+    op_stack[++op_top] = c;
+}
+
+char pop_op() {
+    return op_stack[op_top--];
+}
 
 int precedence(char op) {
     if (op == '+' || op == '-') return 1;
@@ -38,13 +58,8 @@ void solve_top() {
 
 int evaluate(char *s) {
     int i = 0;
-
     while (s[i]) {
-        if (isspace(s[i])) {
-            i++;
-            continue;
-        }
-
+        if (isspace(s[i])) { i++; continue; }
         if (isdigit(s[i])) {
             int num = 0;
             while (isdigit(s[i])) {
@@ -54,50 +69,43 @@ int evaluate(char *s) {
             push_num(num);
             continue;
         }
-
         if (s[i] == '(') {
             push_op(s[i]);
             i++;
             continue;
         }
-
         if (s[i] == ')') {
             while (op_top >= 0 && op_stack[op_top] != '(')
                 solve_top();
-            pop_op(); 
+            pop_op();
             i++;
             continue;
         }
-
-        if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i] == '%') {
+        if (s[i]=='+'||s[i]=='-'||s[i]=='*'||s[i]=='/'||s[i]=='%') {
             while (op_top >= 0 && precedence(op_stack[op_top]) >= precedence(s[i]))
                 solve_top();
             push_op(s[i]);
             i++;
             continue;
         }
-
         i++;
     }
-
     while (op_top >= 0)
         solve_top();
-
     return pop_num();
 }
 
 int main() {
-    char line[256];
-
-    printf("Infix Calculator (Ctrl+D to exit)\n");
-
-    while (printf("> "), fflush(stdout), fgets(line, sizeof(line), stdin)) {
+    char *line = NULL;
+    size_t len = 0;
+    printf("Calculator (Ctrl+D to exit)\n");
+    while (printf("> "), fflush(stdout), getline(&line, &len, stdin) != -1) {
         num_top = op_top = -1;
-        int result = evaluate(line);
-        printf("= %d\n", result);
+        printf("= %d\n", evaluate(line));
     }
-
-    printf("\nExiting.\n");
+    free(line);
+    free(num_stack);
+    free(op_stack);
     return 0;
 }
 
